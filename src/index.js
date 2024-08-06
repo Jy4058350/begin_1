b60ca26c760929134c924a8b400f4f026c9397ee
Jy4058350 <j_yamabe@mac.com>
2024-08-06 13:41:27 +0900

    親と子

--- src/index.js
+++ src/index.js
@@ -5,25 +5,21 @@ import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const sampleText = "サンプルテキスト"
-const cssVariable = {
-  fontSize: "1.25rem",
-  fontWeight: 600,
+
+const ParentChild = () => {
+  return (
+    <>
+      親です。
+      <ChildComponent />
+    </>
+  )
 }
 
-const handleClick = e => console.log("ボタンがクリックされました")
-const handleClick2 = e => console.log(e.target.textContent)
-const handleClick3 = init => console.log(`引数は${init}だよ`)
+const ChildComponent = () => {
+  return <>子です</>
+}
 
-const element = (
-  <>
-    <button onClick={handleClick}>ボタン</button>
-    <button onClick={handleClick2}>ボタン2だよ</button>
-    <button onClick={() => handleClick3(3)}>ボタン3だよ</button>
-  </>
-)
-
-root.render(element)
+root.render(<ParentChild />)
 // root.render(
 //   <React.StrictMode>
 //     <App />
