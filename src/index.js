8e1babfb03f446100dc463662dac6a86b42905ff
Jy4058350 <j_yamabe@mac.com>
2024-08-07 16:46:48 +0900

    default設定

--- src/index.js
+++ src/index.js
@@ -8,61 +8,17 @@ import { list } from "postcss"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const ParentComponent = () => {
-  const members = [
-    {
-      id: 1,
-      name: "伊藤",
-    },
-    {
-      id: 2,
-      name: "佐藤",
-    },
-    {
-      id: 3,
-      name: "高橋",
-    },
-  ]
   return (
     <>
-      親です。
-      <ChildComponent members={members} /> <br />
-      コンポーネントを複数表示
-      <br />
-      {members.map(member => (
-        <MemberList key={member.id} name={member.name} />
-        // <MemberList name={member.name} />
-      ))}
+      親です <br />
+      <ChildComponent name="douan" />
     </>
   )
 }
-
-const MemberList = props => {
-  return (
-    <>
-      <div>{props.name}</div>
-    </>
-  )
+const ChildComponent = ({ name = "test" }) => {
+  return <>子です。 {name}</>
 }
-
-const ChildComponent = props => {
-  console.log("props", props.members[0])
-  return (
-    <>
-      こどもは{props.members[0].name}です
-      <ul>
-        {props.members.map(member => (
-          <li key={member.id}>{member.name}</li>
-        ))}
-      </ul>
-    </>
-  )
-}
-
-root.render(
-  <React.StrictMode>
-    <ParentComponent />
-  </React.StrictMode>
-)
+root.render(<ParentComponent />)
 // root.render(
 //   <React.StrictMode>
 //     <App />
