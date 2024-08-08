0b42e0320ce43eb30c066198cc4f6f43ad61fb20
Jy4058350 <j_yamabe@mac.com>
2024-08-08 10:05:26 +0900

    useStateの挙動確認

--- src/index.js
+++ src/index.js
@@ -7,15 +7,19 @@ import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const UseState = () => {
+const TestUseState = () => {
   const [count, setCount] = useState(0)
+  console.log(`コンポーネント表示${count}`)
   const handleClick = () => {
     setCount(count + 1)
+    console.log(`関数内での表示${count}`)
   }
   return (
     <>
-      useState <br />
-      カウント：{count} <br />
+      test useState
+      <br />
+      count is {count}
+      <br />
       <button onClick={handleClick}>ボタン</button>
     </>
   )
@@ -23,11 +27,6 @@ const UseState = () => {
 
 root.render(
   <React.StrictMode>
-    <UseState />
+    <TestUseState />
   </React.StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
