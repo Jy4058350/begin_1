2602d6c0494b6ee39a48ebcd078e55c00d61eb14
Jy4058350 <j_yamabe@mac.com>
2024-08-08 11:09:16 +0900

    子から親のstate操作

--- src/index.js
+++ src/index.js
@@ -6,24 +6,29 @@ import reportWebVitals from "./reportWebVitals"
 // import { list } from "postcss"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(root)
 
 const TestComponent = () => {
   const [count, setCount] = useState(0)
   const handleEvent = () => {
-    setCount(count => {
-      console.log(count)
-      return count + 1
-    })
+    setCount(count + 1)
   }
   return (
     <>
       testUseState <br />
+      count: {count} <br />
       <button onClick={handleEvent}>ボタン</button>
+      <ChildComponent handleClick={handleEvent} />
     </>
   )
 }
-
+const ChildComponent = props => {
+  return (
+    <>
+      child is . <br />
+      <button onClick={props.handleClick}>ぼたん</button>
+    </>
+  )
+}
 root.render(
   <React.StrictMode>
     <TestComponent />
