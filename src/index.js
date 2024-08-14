db10b4bd49c9ef3c19cbd938a8a7a0190344528a
Jy4058350 <j_yamabe@mac.com>
2024-08-14 09:00:20 +0900

    carousel-first

--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import "./styles.css"
-
-const app = ReactDOM.createRoot(document.getElementById("app"))
-
-const ModalWindow = () => {
-  const [isShow, setIsShow] = useState(true)
-  const toggleShow = () => setIsShow(!isShow)
-  console.log(isShow)
-
-  return (
-    <>
-      <button onClick={toggleShow}>画像を表示</button>
-      <div style={{ display: isShow ? "initial" : "none" }}>
-        <div className="modal__inner">
-          <img src="./image001.jpg" alt="画像" />
-          <div onClick={() => setIsShow(!isShow)} className="modal__close">
-            <i className="fas fa-times"></i>
-          </div>
-        </div>
-        <div onClick={() => setIsShow(!isShow)} className="modal__background"></div>
-      </div>
-    </>
-  )
-}
-
-app.render(<ModalWindow />)
