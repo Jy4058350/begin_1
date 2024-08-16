b04460d66d60f9a20f646c6ae48557a4b552a82b
Jy4058350 <j_yamabe@mac.com>
2024-08-17 07:32:31 +0900

    feat:blur

--- src/index.js
+++ src/index.js
@@ -8,15 +8,18 @@ const App = () => {
   const [text, setText] = useState("こんにちは")
 
   const handleChange = e => {
-    const value = e.target.value
-    //ここに三項演算子をつかって、valueが空文字の場合は「未入力」と表示するようにしてください
+    setText(e.target.value)
+  }
 
-    e.target.value ? setText(value) : setText("未入力")
+  const handleBlur = () => {
+    if (text === "") {
+      setText("こんにちは")
+    }
   }
 
   return (
     <>
-      <input id="text" name="text" value={text} onChange={handleChange} onClick={() => setText("")} />
+      <input id="text" name="text" value={text} onChange={handleChange} onClick={() => setText("")} onBlur={handleBlur} />
       <div>入力テキスト：{text}</div>
       <button className="m-4 bg-gray-300 text-white px-3 py-2 rounded-md" onClick={() => setText("こんにちは")}>
         rest
