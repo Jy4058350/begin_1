ec6dd0c8d8e841ed14c278067ccdfc3688429e1f
Jy4058350 <j_yamabe@mac.com>
2024-08-17 08:42:46 +0900

    self-product

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import React, { useState } from "react"
 import ReactDOM from "react-dom/client"
 import "./styles.css"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const Checkbox = () => {
+  const [text, setText] = useState("You did not like this")
+  const handleCheck = e => {
+    setText(e.target.checked ? "You liked this" : "You did not like this")
+  }
+  return (
+    <>
+      <label>
+        <input type="checkbox" onChange={handleCheck} /> I like this?
+      </label>
+      <p>{text}</p>
+    </>
+  )
+}
+
+root.render(<Checkbox />)
