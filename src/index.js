693a17831a16a910cb5d8aa871371c3031bb4783
Jy4058350 <j_yamabe@mac.com>
2024-08-17 10:29:24 +0900

    more simple

--- src/index.js
+++ src/index.js
@@ -6,25 +6,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const MyForm = () => {
   const [name, setName] = useState("You")
-  const handleName = e => {
-    setName(e.target.value)
-  }
 
   const [age, setAge] = useState(42)
-  const handleClick = e => {
-    setAge(age + 1)
-  }
 
   return (
     <>
       <div className="m-8">
-        <input className=" mb-4 border border-gray-200" type="text" value={name} onChange={handleName} />
-        <p className="mb-4">
-          Hello {name} are {age}
-        </p>
-        <button className="bg-gray-300 text-white p-4 rounded-md" onClick={handleClick}>
+        <input className=" block mb-4 border border-gray-200" type="text" value={name} onChange={e => setName(e.target.value)} />
+        <button className="mb-4 bg-gray-300 text-white px-4 py-2 rounded-md" onClick={() => setAge(age + 1)}>
           Increment age
         </button>
+        <p className="">
+          Hello {name} are {age}
+        </p>
       </div>
     </>
   )
