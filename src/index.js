2e0eb94fc1cef4d0a1efc2e1670adcbd0f401e0e
Jy4058350 <j_yamabe@mac.com>
2024-08-19 12:24:19 +0900

    self-product

--- src/index.js
+++ src/index.js
@@ -14,26 +14,71 @@ const Form = () => {
     },
   })
 
-  const handleChange = e => {}
+  const handleChange1 = e => {
+    e.target.name = e.target.value
+    setPerson({
+      ...person,
+      name: e.target.name,
+    })
+  }
+
+  const handleChange2 = e => {
+    e.target.name = e.target.value
+    const newArtwork = {
+      ...person.artwork,
+      title: e.target.name,
+    }
+    const newPerson = {
+      ...person,
+      artwork: newArtwork,
+    }
+    setPerson(newPerson)
+  }
+
+  const handleChange3 = e => {
+    e.target.name = e.target.value
+    const newArtwork = {
+      ...person.artwork,
+      city: e.target.name,
+    }
+    const newPerson = {
+      ...person,
+      artwork: newArtwork,
+    }
+    setPerson(newPerson)
+  }
+
+  const handleChange4 = e => {
+    e.target.name = e.target.value
+    const newArtwork = {
+      ...person.artwork,
+      image: e.target.name,
+    }
+    const newPerson = {
+      ...person,
+      artwork: newArtwork,
+    }
+    setPerson(newPerson)
+  }
   return (
     <>
       <label className="block mb-4">
         Name:
-        <input className="ml-4 border-2 border-gray-500 text-xs" name="name" value={person.name} onChange={handleChange} />
+        <input className="ml-4 border-2 border-gray-500 text-xs" name="name" value={person.name} onChange={handleChange1} />
       </label>
       <label className="block mb-4">
         Title:
-        <input className="ml-4 border-2 border-gray-500 text-xs" name="title" value={person.artwork.title} onChange={handleChange} />
+        <input className="ml-4 border-2 border-gray-500 text-xs" name="title" value={person.artwork.title} onChange={handleChange2} />
       </label>
       <label className="block mb-4">
         City:
-        <input className="ml-4 border-2 border-gray-500 text-xs" name="city" value={person.artwork.city} onChange={handleChange} />
+        <input className="ml-4 border-2 border-gray-500 text-xs" name="city" value={person.artwork.city} onChange={handleChange3} />
       </label>
       <label className="block mb-4">
         Image:
-        <input className="ml-4 border-2 border-gray-500 text-xs" name="image" value={person.artwork.image} />
+        <input className="ml-4 border-2 border-gray-500 text-xs" name="image" value={person.artwork.image} onChange={handleChange4} />
       </label>
-      Blue Nana By Niki de Saint Phalle (located in {})
+      {person.artwork.title} By {person.name} (located in {person.artwork.city})
       <img className="mt-4 w-48" src={person.artwork.image} alt="" />
     </>
   )
