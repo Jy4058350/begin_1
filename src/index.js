097564a4fe8a05feb00f29bd32de3276e5bfd635
Jy4058350 <j_yamabe@mac.com>
2024-08-19 10:50:37 +0900

    冗長な書き方

--- src/index.js
+++ src/index.js
@@ -4,37 +4,54 @@ import "./styles.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
-const MovingDot = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 })
+const Form = () => {
+  const [name, setName] = useState({
+    first: "山田",
+    last: "太郎",
+    email: "test@test.com",
+  })
 
+  const handlefirstNameChange = e => {
+    setName({
+      first: e.target.value,
+      last: name.last,
+      email: name.email,
+    })
+  }
+  const handleLastNameChange = e => {
+    setName({
+      first: name.first,
+      last: e.target.value,
+      emai: name.email,
+    })
+  }
+  const handleEmailChange = e => {
+    setName({
+      first: name.first,
+      last: name.last,
+      email: e.target.value,
+    })
+  }
   return (
-    <div
-      onPointerMove={e => {
-        const nextPosition = {}
-        nextPosition.x = e.clientX
-        nextPosition.y = e.clientY
-        setPosition(nextPosition)
-      }}
-      style={{
-        position: "relative",
-        width: "100vw",
-        height: "100vh",
-      }}
-    >
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "red",
-          borderRadius: "50%",
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          left: -10,
-          top: -10,
-          width: 20,
-          height: 20,
-        }}
-      />
+    <div>
+      <label className="block" htmlFor="name1">
+        first name:
+        <input type="text" id="name1" name="first" value={name.first} onChange={handlefirstNameChange} />
+      </label>
+      <label className="block" htmlFor="name2">
+        last name:
+        <input type="text" id="name2" name="last" value={name.last} onChange={handleLastNameChange} />
+      </label>
+      <label className="block" htmlFor="email">
+        email:
+        <input type="email" id="email" name="email" value={name.email} onChange={handleEmailChange} />
+      </label>
+
+      <div>
+        苗字は{name.first}です 名前は{name.last}です メールアドレスは{name.email}です
+      </div>
     </div>
   )
 }
 
-root.render(<MovingDot />)
+root.render(<Form />)
