9837457f33bbb903af0874916ab06a7b6dd94bce
Jy4058350 <j_yamabe@mac.com>
2024-08-19 11:00:32 +0900

    stateの変数と関数名を変更

--- src/index.js
+++ src/index.js
@@ -5,47 +5,48 @@ import "./styles.css"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const Form = () => {
-  const [name, setName] = useState({
+  const [person, setPerson] = useState({
     first: "山田",
     last: "太郎",
     email: "test@test.com",
   })
 
   const handlefirstNameChange = e => {
-    setName({
-      ...name,
+    setPerson({
+      ...person,
+      ...person,
       first: e.target.value,
     })
   }
   const handleLastNameChange = e => {
-    setName({
-      ...name,
+    setPerson({
+      ...person,
       last: e.target.value,
     })
   }
   const handleEmailChange = e => {
-    setName({
-      ...name,
+    setPerson({
+      ...person,
       email: e.target.value,
     })
   }
   return (
     <div>
       <label className="block" htmlFor="name1">
-        first name:
-        <input type="text" id="name1" name="first" value={name.first} onChange={handlefirstNameChange} />
+        first person:
+        <input type="text" id="name1" person="first" value={person.first} onChange={handlefirstNameChange} />
       </label>
       <label className="block" htmlFor="name2">
-        last name:
-        <input type="text" id="name2" name="last" value={name.last} onChange={handleLastNameChange} />
+        last person:
+        <input type="text" id="name2" person="last" value={person.last} onChange={handleLastNameChange} />
       </label>
       <label className="block" htmlFor="email">
         email:
-        <input type="email" id="email" name="email" value={name.email} onChange={handleEmailChange} />
+        <input type="email" id="email" person="email" value={person.email} onChange={handleEmailChange} />
       </label>
 
       <div>
-        苗字は{name.first}です 名前は{name.last}です メールアドレスは{name.email}です
+        苗字は{person.first}です 名前は{person.last}です メールアドレスは{person.email}です
       </div>
     </div>
   )
