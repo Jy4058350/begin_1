b6e45cdc21601bada1dfa2620c487871aa0145c2
Jy4058350 <j_yamabe@mac.com>
2024-08-21 08:41:34 +0900

    self-product

--- src/index.js
+++ src/index.js
@@ -11,12 +11,20 @@ const counters = [
   { id: 2, count: 0 },
 ]
 const CounterList = () => {
+  const [count, setCount] = useState(counters)
+  const handleClick = () => {
+    counters.map(counter => {
+     (c)=>c.id ? count.id :setCount(count + 1):null
+    })
+  }
   return (
     <div>
       {counters.map(counter => (
         <div className="m-4 text-lg" key={counter.id}>
           {counter.count}
-          <button className="ml-2 bg-gray-300 text-gray-700 px-2 text-sm border border-gray-500 rounded-sm ">+1</button>
+          <button className="ml-2 bg-gray-300 text-gray-700 px-2 text-sm border border-gray-500 rounded-sm " onClick={handleClick}>
+            +1
+          </button>
         </div>
       ))}
     </div>
