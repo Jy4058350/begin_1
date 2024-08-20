69e7b8dd08802304db4a4328b3729e9e1a514296
Jy4058350 <j_yamabe@mac.com>
2024-08-20 20:57:19 +0900

    pushを使った悪い例

--- src/index.js
+++ src/index.js
@@ -3,3 +3,36 @@ import { useState } from "react"
 import { useImmer } from "use-immer"
 import ReactDOM from "react-dom/client"
 import "./styles.css"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+let nextId = 0
+const List = () => {
+  const [name, setName] = useState("")
+  const [artists, setArtists] = useState([])
+
+  const handleChange = e => {
+    setName(e.target.value)
+  }
+  const handleClick = () => {
+    artists.push({
+      id: nextId++,
+      name: name,
+    })
+  }
+  return (
+    <div className="text-2xl font-semibold mx-4 my-4">
+      Inspiring sculptors:
+      <input className="inline-block border text-xs my-4" name="name" value={name} onChange={handleChange} />
+      <button className=" bg-gray-400 text-black text-sm px-4 py-2 rounded-md" onClick={handleClick}>
+        Add
+      </button>
+      <ul>
+        {artists.map(artist => {
+          return <li key={artist.id}>{artist.name}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+root.render(<List />)
