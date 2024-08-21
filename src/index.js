30d3f4f468aadad761e3b84ba9cf7f41139ecd3f
Jy4058350 <j_yamabe@mac.com>
2024-08-21 11:15:19 +0900

    get e.targe.value

--- src/index.js
+++ src/index.js
@@ -5,32 +5,29 @@ import ReactDOM from "react-dom/client"
 import "./styles.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-const initialCounters = [0, 0, 0]
-const CounterList = () => {
-  const [counters, setCounters] = useState(initialCounters)
-
-  const handleClick = index => {
-    const nextCounters = counters.map((c, i) => {
-      if (i === index) {
-        return c + 1
-      } else {
-        return c
-      }
-    })
-    setCounters(nextCounters)
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+]
+const List = () => {
+  const [artists, setArtists] = useState(initialArtists)
+  const [newArtists, setNewArtists] = useState("")
+  const handleChange = e => {
+    setNewArtists(e.target.value)
   }
+
   return (
-    <ul>
-      {counters.map((counter, i) => (
-        <li className="m-4 text-lg" key={i}>
-          {counter}
-          <button className="ml-2 bg-gray-300 text-gray-700 px-2 text-sm border border-gray-500 rounded-sm " onClick={() => handleClick(i)}>
-            +1
-          </button>
-        </li>
-      ))}
-    </ul>
+    <div className="text-2xl font-semibold">
+      Inspiring sculptors:
+      <input className="mt-2 border-2 text-sm border-gray-300" value={newArtists} onChange={handleChange}></input>
+      <button className="ml-2 bg-gray-400 text-gray-600 px-2 border-2 border-gray-600 font-light rounded-md text-sm">Insert</button>
+      <ul className="mt-4 text-md text-lg font-light text-gray-700">
+        {artists.map(artist => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
-
-root.render(<CounterList />)
+root.render(<List />)
