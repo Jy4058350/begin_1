a6016bc82a1432789e4e80f9a2a0405b3250b69e
Jy4058350 <j_yamabe@mac.com>
2024-08-22 06:16:28 +0900

    修正その２ーuseStateを２つに増やし、myとyourをそれぞれ準備

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ import "./styles.css"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const initialList = [
-  { id: 0, title: "Big Bellies" },
-  { id: 1, title: "Lunar Landscape" },
-  { id: 2, title: "Terracotta Army" },
+  { id: 0, title: "Big Bellies", seen: false },
+  { id: 1, title: "Lunar Landscape", seen: false },
+  { id: 2, title: "Terracotta Army", seen: true },
 ]
 const List = () => {
-  const [arts, setArts] = useState(initialList)
+  const [myList, setMyList] = useState(initialList)
+  const [yourList, setYourList] = useState(initialList)
 
   const handleChange = i => {}
 
@@ -21,20 +22,21 @@ const List = () => {
       <h1 className="mb-2 text-2xl font-bold">Art Bucket List</h1>
 
       <h2 className="mb-2 text-xl font-normal">My list of art to see:</h2>
-      <ul>
-        {arts.map((art, i) => {
+      {/* <ul> */}
+      <ItemList />
+      {/* {myList.map((art, i) => {
           return (
             <div key={art.id}>
               <input type="checkbox" name={art.title} onChange={handleChange} className="mb-2 text-xs font-normal" />
               <label>{art.title}</label>
             </div>
           )
-        })}
-      </ul>
+        })} */}
+      {/* </ul> */}
 
       <h2 className="mb-2 text-xl font-normal">Your list of art to see:</h2>
       <ul>
-        {arts.map(art => {
+        {myList.map(art => {
           return (
             <div key={art.id}>
               <input type="checkbox" className="mb-2 text-xs font-normal" />
@@ -44,6 +46,22 @@ const List = () => {
         })}
       </ul>
     </div>
+  )
+}
+
+const ItemList = (myList, handleChange) => {
+  console.log(myList)
+  return (
+    <ul>
+      {/* {myList.map(art => {
+        return (
+          <div key={art.id}>
+            <input type="checkbox" name={art.title} onChange={handleChange} className="mb-2 text-xs font-normal" />
+            <label>{art.title}</label>
+          </div>
+        )
+      })} */}
+    </ul>
   )
 }
 
