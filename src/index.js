7af2dcda3dfd857afa7b715ecc2f9306a02f5b65
Jy4058350 <j_yamabe@mac.com>
2024-08-21 15:14:20 +0900

    style

--- src/index.js
+++ src/index.js
@@ -3,3 +3,34 @@ import { useState } from "react"
 import { useImmer } from "use-immer"
 import ReactDOM from "react-dom/client"
 import "./styles.css"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+const initialList = [
+  {
+    id: 0,
+    name: "Big Bellies",
+  },
+  { id: 1, name: "Lunar Landscape" },
+  {
+    id: 2,
+    name: "Terracotta Army",
+  },
+]
+const List = () => {
+  const [names, setNames] = useState(initialList)
+  return (
+    <div>
+      <button className="m-4 bg-gray-300 text-gray-800 px-2 border border-gray-800 text-sm rounded-md">Reverse</button>
+      <ul>
+        {names.map(name => (
+          <li className="ml-4 text-xl font-light" key={name.id}>
+            {name.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+root.render(<List />)
