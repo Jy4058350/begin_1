a10a153c7846dae43704dfec21d64e8cb9271793
Jy4058350 <j_yamabe@mac.com>
2024-08-23 08:39:33 +0900

    testStart

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,7 +1,3 @@
-function Profile() {
-  return <img src="https://i.imgur.com/QIrZWGIs.jpg" alt="Alan L. Hart" />
-}
-
 export default function Gallery() {
   return (
     <section>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client"
-import App from "./App.js"
+import Bio from "./App.js"
 
 const app = ReactDOM.createRoot(document.getElementById("app"))
 
-app.render(<App />)
+app.render(<Bio />)
