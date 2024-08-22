7a7162fbb41704dadc967250cc9f6adb70fd9098
Jy4058350 <j_yamabe@mac.com>
2024-08-22 23:12:55 +0900

    style

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./styles.css"
 
 function Profile() {
-  return <img className="m-4" src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />
+  return <img className="m-2 w-20" src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherine Johnson" />
 }
 
 export default function Gallery() {
   return (
     <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <h1 className="m-2 font-bold text-2xl">Amazing scientists</h1>
+      <div className="flex flex-row">
+        <Profile />
+        <Profile />
+        <Profile />
+      </div>
     </section>
   )
 }
