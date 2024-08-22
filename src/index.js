ca051e88383073afc63234381f86feb801e50f86
Jy4058350 <j_yamabe@mac.com>
2024-08-22 10:39:30 +0900

    handler関数の書き方がわからないので、回答みていく

--- src/index.js
+++ src/index.js
@@ -23,19 +23,19 @@ const List = () => {
     <div className="m-4">
       <h1 className="mb-2 text-2xl font-bold">Art Bucket List</h1>
       <h2 className="mb-2 text-xl font-normal">My list of art to see:</h2>
-      <ItemList artworks={myList} />
+      <ItemList artworks={myList} toggle={handleToggleMyList} />
       <h2 className="mt-4 mb-2 text-xl font-normal">Your list of art to see:</h2>
-      <ItemList artworks={yourList} />
+      <ItemList artworks={yourList} toggle={handleToggleYourList} />
     </div>
   )
 }
 
-const ItemList = ({ artworks }) => {
+const ItemList = ({ artworks, toggle }) => {
   return (
     <ul className="mt-4">
       {artworks.map(artwork => (
         <li key={artwork.id}>
-          <input type="checkbox" checked={artwork.seen} className="m-2" />
+          <input type="checkbox" checked={artwork.seen} onChange={toggle} className="m-2" />
           {artwork.title}
         </li>
       ))}
