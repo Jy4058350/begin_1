248e9d7652be0e1b7d68843ddb7021151d52cf79
Jy4058350 <j_yamabe@mac.com>
2024-08-25 07:51:21 +0900

    コンポーネントにpropsを渡していないのでうまくいかない

--- src/App.js
+++ src/App.js
@@ -1,3 +1,27 @@
 export default function App() {
-  return <></>
+  return (
+    <>
+      <Avatar person />
+    </>
+  )
+}
+
+const person = {
+  url: "https://i.imgur.com/1bX5QH6.jpg",
+  name: "Lin Lanying",
+  width: "100",
+  height: "100",
+}
+
+function Avatar(person) {
+  console.log(person)
+  const avatar = person.url
+  const description = person.name
+  const w = person.width
+  const h = person.height
+  return (
+    <>
+      <img src={avatar} alt={description} width={w} height={h} />
+    </>
+  )
 }
