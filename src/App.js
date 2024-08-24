2db165169703441dbc93bc6118a71d3394784bfe
Jy4058350 <j_yamabe@mac.com>
2024-08-25 07:19:35 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,36 +1,3 @@
 export default function App() {
-  return (
-    <>
-      <TodoList />
-    </>
-  )
-}
-
-const baseUrl = "https://i.imgur.com/"
-const person = {
-  name: "Gregorio Y. Zara",
-  imageId: "7vQD0fP",
-  imageSize: "b",
-  theme: {
-    backgroundColor: "black",
-    color: "pink",
-  },
-}
-
-function TodoList() {
-  return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
-      <img
-        className="avatar"
-        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
-        alt={person.name}
-      />
-      <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
-      </ul>
-    </div>
-  )
+  return <></>
 }
