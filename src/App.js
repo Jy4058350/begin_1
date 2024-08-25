00e5d82ea8afb265ded80efb8a91b7c99797723f
Jy4058350 <j_yamabe@mac.com>
2024-08-25 09:32:25 +0900

    childrenとしてJSXを渡す

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import { getImageUrl } from './utils.js'
+import Avatar from './Avatar.js'
 
 export default function App() {
   return (
     <>
-      {' '}
-      <Avator person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }} />
-      <Avator
-        person={{ name: 'Katsuko Saruhashi', imageId: 'YfeOqp2' }}
-        size={50}
-      />
-      <Avator person={{ name: 'Aklilu Lemma', imageId: 'OKS67lh' }} size={80} />
+      <Card>
+        <Avatar
+          person={{
+            name: 'Katsuko Saruhashi',
+            imageId: 'YfeOqp2',
+          }}
+        />
+        <Avatar
+          person={{
+            name: 'Katsuko Saruhashi',
+            imageId: 'YfeOqp2',
+          }}
+        />
+        <Avatar
+          person={{
+            name: 'Katsuko Saruhashi',
+            imageId: 'YfeOqp2',
+          }}
+        />
+      </Card>
     </>
   )
 }
 
-function Avator({ person, size = 100 }) {
-  return (
-    <img
-      className='avatar'
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  )
+function Card({ children }) {
+  return <div className='card'>{children}</div>
 }
