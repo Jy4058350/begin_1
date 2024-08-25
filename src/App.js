27d7dc9685aafe1d9b66986800397f78c66afd98
Jy4058350 <j_yamabe@mac.com>
2024-08-25 11:30:35 +0900

    firstチャレンジ３の問題

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import { getImageUrl } from './utils.js'
-
-function Avatar({ person, size }) {
-  const select = size > 90 ? 'b' : 's'
-  return (
-    <img
-      className='avatar'
-      src={getImageUrl(person, select)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  )
-}
-
 export default function App() {
   return (
-    <>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Gregorio Y. Zara',
-          imageId: '7vQD0fP',
-        }}
-      />
-      <Avatar
-        size={40}
-        person={{
-          name: 'Gregorio Y. Zara',
-          imageId: '7vQD0fP',
-        }}
-      />
-    </>
+    <div>
+      <div className='card'>
+        <div className='card-content'>
+          <h1>Photo</h1>
+          <img
+            className='avatar'
+            src='https://i.imgur.com/OKS67lhm.jpg'
+            alt='Aklilu Lemma'
+            width={70}
+            height={70}
+          />
+        </div>
+      </div>
+      <div className='card'>
+        <div className='card-content'>
+          <h1>About</h1>
+          <p>
+            Aklilu Lemma was a distinguished Ethiopian scientist who discovered
+            a natural treatment to schistosomiasis.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
