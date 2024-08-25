090b4bcc717a4f4ddff374ca9480d177e3d3ce73
Jy4058350 <j_yamabe@mac.com>
2024-08-25 11:15:18 +0900

    first チャレンジ問題−2

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
 import { getImageUrl } from './utils.js'
 
-export default function App() {
+function Avatar({ person, size }) {
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        name='Maria Skłodowska-Curie'
-        imageId='szV5sdG'
-        job='physicist and chemist'
-        awards={[
-          'Nobel Prize in Physics',
-          'Nobel Prize in Chemistry',
-          'Davy Medal',
-          'Matteucci Medal',
-        ]}
-        discoveredDescription='polonium (chemical element)'
-      />
-      <Profile
-        name='Katsuko Saruhashi'
-        imageId='YfeOqp2'
-        job={'geochemist'}
-        awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
-        discoveredDescription={
-          'a method for measuring carbon dioxide in seawater'
-        }
-      />
-    </div>
+    <img
+      className='avatar'
+      src={getImageUrl(person, 'b')}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
   )
 }
 
-function Profile({
-  name,
-  imageId,
-  job,
-  awards,
-  discoveredDescription,
-  size = 70,
-}) {
+export default function App() {
   return (
-    <section className='profile'>
-      <h2>
-        {name}
-        <img
-          className='avatar'
-          src={getImageUrl(imageId)}
-          alt={name}
-          width={size}
-          height={size}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            {job}
-          </li>
-          <li>
-            <b>Awards:{awards.length}</b>
-            {awards}
-          </li>
-          <li>
-            <b>Discovered: </b>
-            {discoveredDescription}
-          </li>
-        </ul>
-      </h2>
-    </section>
+    <Avatar
+      size={40}
+      person={{
+        name: 'Gregorio Y. Zara',
+        imageId: '7vQD0fP',
+      }}
+    />
   )
 }
