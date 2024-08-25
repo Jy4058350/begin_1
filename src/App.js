bf785fa4ebc874b39a434fe4e009663555d60917
Jy4058350 <j_yamabe@mac.com>
2024-08-25 15:34:25 +0900

    flagをつかう

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 export default function App() {
-  return <></>
+  return (
+    <>
+      <h2>Sally Ride's Packing List</h2>
+      <PackingList />
+    </>
+  )
+}
+
+function PackingList() {
+  const Items = [
+    {
+      id: 0,
+      product: 'Space suit',
+      isPacked: true,
+    },
+    {
+      id: 1,
+      product: 'Helmet with a golden leaf',
+      isPacked: true,
+    },
+    { id: 2, product: 'Photo of Tam', isPacked: true },
+  ]
+
+  return (
+    <>
+      <ul>
+        {Items.map(item => (
+          <li key={item.id}>{item.product}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
