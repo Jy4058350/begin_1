9d557bc045b18f3787d3446970aea00b6ed4fc63
Jy4058350 <j_yamabe@mac.com>
2024-08-25 17:44:34 +0900

    self-product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 function Drink({ name }) {
+  let plant, content, age
+  if (name === 'tea') {
+    plant = 'leaf'
+    content = '15–70 mg/cup'
+    age = '4,000+ years'
+  } else {
+    plant = 'bean'
+    content = '80–185 mg/cup'
+    age = '1,000+ years'
+  }
   return (
     <section>
       <h1>{name}</h1>
       <dl>
         <dt>Part of plant</dt>
-        <dd>{name === 'tea' ? 'leaf' : 'bean'}</dd>
+        <dd>{plant}</dd>
+
         <dt>Caffeine content</dt>
-        <dd>{name === 'tea' ? '15–70 mg/cup' : '80–185 mg/cup'}</dd>
+        <dd>{content}</dd>
         <dt>Age</dt>
-        <dd>{name === 'tea' ? '4,000+ years' : '1,000+ years'}</dd>
+        <dd>{age}</dd>
       </dl>
     </section>
   )
