697509879628b86a21bdf20cbce3e01516605ce3
Jy4058350 <j_yamabe@mac.com>
2024-08-25 11:28:59 +0900

    チャレンジ２の問題を三項演算子で書いた

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { getImageUrl } from './utils.js'
 
 function Avatar({ person, size }) {
+  const select = size > 90 ? 'b' : 's'
   return (
     <img
       className='avatar'
-      src={getImageUrl(person, 'b')}
+      src={getImageUrl(person, select)}
       alt={person.name}
       width={size}
       height={size}
@@ -14,12 +15,21 @@ function Avatar({ person, size }) {
 
 export default function App() {
   return (
-    <Avatar
-      size={40}
-      person={{
-        name: 'Gregorio Y. Zara',
-        imageId: '7vQD0fP',
-      }}
-    />
+    <>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Gregorio Y. Zara',
+          imageId: '7vQD0fP',
+        }}
+      />
+      <Avatar
+        size={40}
+        person={{
+          name: 'Gregorio Y. Zara',
+          imageId: '7vQD0fP',
+        }}
+      />
+    </>
   )
 }
