350904c9adebd81c4c23db0ba0d5594cbe2a4bde
Jy4058350 <j_yamabe@mac.com>
2024-08-25 10:10:19 +0900

    self-product

--- src/App.js
+++ src/App.js
@@ -4,54 +4,64 @@ export default function App() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className='profile'>
-        <h2>Maria Skłodowska-Curie</h2>
+      <Profile
+        size={70}
+        person={{ name: 'Maria Skłodowska-Curie', imageId: 'szV5sdG' }}
+        job={'physicist and chemist'}
+        award={4}
+        awardDescription={
+          'Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal'
+        }
+        discoveredDescription={'polonium (chemical element)'}
+      />
+      <Profile
+        size={70}
+        person={{ name: 'Katsuko Saruhashi', imageId: 'YfeOqp2' }}
+        job={'geochemist'}
+        award={2}
+        awardDescription={'Miyake Prize for geochemistry, Tanaka Prize'}
+        discoveredDescription={
+          'a method for measuring carbon dioxide in seawater'
+        }
+      />
+    </div>
+  )
+}
+
+function Profile({
+  size,
+  person,
+  job,
+  award,
+  awardDescription,
+  discoveredDescription,
+}) {
+  return (
+    <section className='profile'>
+      <h2>
+        {person.name}
         <img
           className='avatar'
-          src={getImageUrl('szV5sdG')}
-          alt='Maria Skłodowska-Curie'
-          width={70}
-          height={70}
+          src={getImageUrl(person.imageId)}
+          alt={person.name}
+          width={size}
+          height={size}
         />
         <ul>
           <li>
             <b>Profession: </b>
-            physicist and chemist
+            {job}
           </li>
           <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
-            Matteucci Medal)
+            <b>Awards:{award}</b>
+            {awardDescription}
           </li>
           <li>
             <b>Discovered: </b>
-            polonium (chemical element)
+            {discoveredDescription}
           </li>
         </ul>
-      </section>
-      <section className='profile'>
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className='avatar'
-          src={getImageUrl('YfeOqp2')}
-          alt='Katsuko Saruhashi'
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
-    </div>
+      </h2>
+    </section>
   )
 }
