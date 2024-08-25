c618c997c728c7d795630476dbf5f212d25c0500
Jy4058350 <j_yamabe@mac.com>
2024-08-25 15:52:20 +0900

    オブジェクトを直接レンダーしようとするとうまくいかない。かわりに文字列として評価する必要がある。

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 export default function App() {
-  return (
-    <>
-      <h2>Sally Ride's Packing List</h2>
-      <PackingList />
-    </>
-  )
-}
-
-function PackingList() {
   const Items = [
     {
       id: 0,
@@ -19,14 +10,20 @@ function PackingList() {
       product: 'Helmet with a golden leaf',
       isPacked: true,
     },
-    { id: 2, product: 'Photo of Tam', isPacked: true },
+    {
+      id: 2,
+      product: 'Photo of Tam',
+      isPacked: false,
+    },
   ]
 
   return (
     <>
       <ul>
         {Items.map(item => (
-          <li key={item.id}>{item.product}</li>
+          <li key={item.id}>
+            {item.isPacked ? `${item.product}'✔︎'` : `${item.product}`}
+          </li>
         ))}
       </ul>
     </>
