40fde0d8f93fb55ebd2adf66c36000f6d1b9a3e4
Jy4058350 <j_yamabe@mac.com>
2024-08-25 15:12:10 +0900

    self-product

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 export default function App() {
   return (
     <div>
-      <div className='card'>
-        <div className='card-content'>
-          <h1>Photo</h1>
-          <img
-            className='avatar'
-            src='https://i.imgur.com/OKS67lhm.jpg'
-            alt='Aklilu Lemma'
-            width={70}
-            height={70}
-          />
-        </div>
-      </div>
-      <div className='card'>
-        <div className='card-content'>
-          <h1>About</h1>
-          <p>
-            Aklilu Lemma was a distinguished Ethiopian scientist who discovered
-            a natural treatment to schistosomiasis.
-          </p>
-        </div>
-      </div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className='avatar'
+          src='https://i.imgur.com/OKS67lhm.jpg'
+          alt='Aklilu Lemma'
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
     </div>
+  )
+}
+
+function Card({ children }) {
+  return (
+    <>
+      <div className='card'>
+        <div className='card-content'>{children}</div>
+      </div>
+    </>
   )
 }
