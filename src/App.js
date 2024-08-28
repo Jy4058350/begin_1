d89d286a71863da335258511ccf523fb622f7dd8
Jy4058350 <j_yamabe@mac.com>
2024-08-28 11:32:13 +0900

    写真などを追加した配列を使用した実装。

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+function getImageUrl(person) {
+  return 'https://i.imgur.com/' + person.imageId + 's.jpg'
+}
+
 export default function App() {
   const chemists = people.filter(person => person.profession === 'chemist')
   const listItems = chemists.map(person => (
-    <li key={person.id}>{person.name}</li>
+    <li>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
   ))
   return <ul>{listItems}</ul>
 }
@@ -11,25 +22,36 @@ const people = [
     id: 0,
     name: 'Creola Katherine Johnson: mathematician',
     profession: 'mathmatician',
+    accomplishment: 'spaceflight calculations',
+    imageId: 'MK3eW3A',
   },
   {
     id: 1,
-    name: 'Mario José Molina-Pasquel Henríquez: chemist',
+    name: 'Mario José Molina-Pasquel Henríquez:',
     profession: 'chemist',
+    accomplishment: 'discovery of Arctic ozone hole',
+    imageId: 'mynHUSa',
   },
   {
     id: 2,
     name: 'Mohammad Abdus Salam: physicist',
     profession: 'physicist',
+    accomplishment: 'electromagnetism theory',
+    imageId: 'bE7W1ji',
   },
   {
     id: 3,
-    name: 'Percy Lavon Julian: chemist',
+    name: 'Percy Lavon Julian',
     profession: 'chemist',
+    accomplishment:
+      'pioneering cortisone drugs, steroids and birth control pills',
+    imageId: 'IOjWm71',
   },
   {
     id: 4,
     name: 'Subrahmanyan Chandrasekhar: astrophysicist',
     profession: 'astrophysicist',
+    accomplishment: 'white dwarf star mass calculations',
+    imageId: 'lrWQx8l',
   },
 ]
