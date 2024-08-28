62ebf3d6eff7bc4c5db8983e46324ee1ee86cf51
Jy4058350 <j_yamabe@mac.com>
2024-08-28 15:46:13 +0900

    self-product fail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import { recipes } from './data.js'
 
 export default function RecipeList() {
-  const recipeList = recipes.map(recipe => {
-    return (
-      <h2 className='mb-8 p-4 text-2xl' key={recipe.id}>
-        {recipe.name}
-        <ul className='mt-2 p-4 text-xl list-disc'>
-          {recipe.ingredients.map(ingredient => (
-            <li className=' text-basic' key={ingredient}>
-              {ingredient}
-            </li>
-          ))}
-        </ul>
-      </h2>
-    )
-  })
+  console.log(recipes)
+  console.log(recipes[0].id)
   return (
     <div>
-      <h1 className='mb-8 text-3xl font-bold '>Recipes</h1>
-      <div>{recipeList}</div>
+      <h1>Recipes</h1>
+      <div>
+        <Recipe recipes={{ id, name, ingredients }} />
+      </div>
     </div>
   )
+}
+
+function Recipe({ recipes }) {
+  console.log(recipes)
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <h1 key={recipe.id}>{recipes.name}</h1>
+      ))}
+    </div>
+  )
+}
+
+{
+  /* <div className='p-4 mt-4'>
+      <h1 className='mt-4 text-2xl font-bold'>Recipes</h1>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h2 className='mt-8 text-lg font-base font-medium'>{recipe.name}</h2>
+          <ul className='p-4 mt-2 text-base font-light list-disc'>
+            {recipe.ingredients.map(ingredient => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div> */
 }
