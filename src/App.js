fd53bc466cbb0a7348cd1d1ab82c051ba33117c4
Jy4058350 <j_yamabe@mac.com>
2024-08-28 16:01:54 +0900

    {...recipe}はrecipeオブジェクトの全てのプロパティを個別のプロパティとして渡している

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import { recipes } from './data.js'
 
+function Recipe({ id, name, ingredients }) {
+  console.log(id, name, ingredients)
+  return <></>
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map(ingredient => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe {...recipe} />
       ))}
     </div>
   )
