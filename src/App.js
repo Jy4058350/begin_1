d6bb140b722d6f6d83c13e184163973c96fa8c34
Jy4058350 <j_yamabe@mac.com>
2024-08-28 10:44:17 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-function Drink({ name }) {
-  let plant, content, age
-  if (name === 'tea') {
-    plant = 'leaf'
-    content = '15–70 mg/cup'
-    age = '4,000+ years'
-  } else {
-    plant = 'bean'
-    content = '80–185 mg/cup'
-    age = '1,000+ years'
-  }
-  return (
-    <section>
-      <h1>{name}</h1>
-      <dl>
-        <dt>Part of plant</dt>
-        <dd>{plant}</dd>
-
-        <dt>Caffeine content</dt>
-        <dd>{content}</dd>
-        <dt>Age</dt>
-        <dd>{age}</dd>
-      </dl>
-    </section>
-  )
-}
-
 export default function App() {
   return (
-    <div>
-      <Drink name='tea' />
-      <Drink name='coffee' />
-    </div>
+    <ul>
+      <li>Creola Katherine Johnson: mathematician</li>
+      <li>Mario José Molina-Pasquel Henríquez: chemist</li>
+      <li>Mohammad Abdus Salam: physicist</li>
+      <li>Percy Lavon Julian: chemist</li>
+      <li>Subrahmanyan Chandrasekhar: astrophysicist</li>
+    </ul>
   )
 }
