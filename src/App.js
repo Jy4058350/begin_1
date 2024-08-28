fe3235474f2f0e9c188323253e5f66256d703497
Jy4058350 <j_yamabe@mac.com>
2024-08-28 12:54:44 +0900

    sefl-product failed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { recipes } from './data.js'
 
 export default function RecipeList() {
-  const recipeList = recipes.map(recipe => (
-    <li key={recipe.id}>{recipe.name}</li>
-  ))
-  const ingredientItems = recipes.map(item => (
-    <li key={item.id}>{item.ingredients}</li>
-  ))
+  const recipeList = recipes.map(recipe => {
+    return (
+      <h2 className='mb-4' key={recipe.id}>
+        {recipe.name}
+        <ul key={recipe.id}>
+          <li className='font-thin'>{recipe.ingredients}</li>
+        </ul>
+      </h2>
+    )
+  })
   return (
     <div>
-      <h1>Recipes</h1>
-      <h2>{recipeList}</h2>
-      <ul>{ingredientItems}</ul>
+      <h1 className='mb-4 font-bold text-2xl'>Recipes</h1>
+      <div>{recipeList}</div>
     </div>
   )
 }
