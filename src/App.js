6b08b4d1ae8f8762556fc8bc9ead16790a926d91
Jy4058350 <j_yamabe@mac.com>
2024-08-28 12:39:55 +0900

    これだと分離して表示してしまう

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { recipes } from './data.js'
 
 export default function RecipeList() {
+  const recipeList = recipes.map(recipe => (
+    <li key={recipe.id}>{recipe.name}</li>
+  ))
+  const ingredientItems = recipes.map(item => (
+    <li key={item.id}>{item.ingredients}</li>
+  ))
   return (
     <div>
       <h1>Recipes</h1>
+      <h2>{recipeList}</h2>
+      <ul>{ingredientItems}</ul>
     </div>
   )
 }
