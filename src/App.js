ce32e91cfeb521336f3a2e810023c71960d80b64
Jy4058350 <j_yamabe@mac.com>
2024-08-28 11:49:45 +0900

    チャレンジ問題１first

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { people } from './data.js'
 import { getImageUrl } from './utils.js'
 
-export default function App() {
-  const chemists = people.filter(person => person.profession === 'chemist')
-  const listItems = chemists.map(person => (
+export default function List() {
+  const listItems = people.map(person => (
     <li key={person.id}>
       <img src={getImageUrl(person)} alt={person.name} />
       <p>
-        <b>{person.name}</b>
+        <b>{person.name}:</b>
         {' ' + person.profession + ' '}
         known for {person.accomplishment}
       </p>
     </li>
   ))
-  return <ul>{listItems}</ul>
+  return (
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
+  )
 }
