55a0241ff83b72f46c780a33186ee6cdaba0b8f3
Jy4058350 <j_yamabe@mac.com>
2024-08-28 16:12:10 +0900

    チャレンジ問題３first

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import { recipes } from './data.js'
-
-function Recipe({ id, name, ingredients }) {
-  console.log(id, name, ingredients)
-  return (
-    <div>
-      <h2>{name}</h2>
-      <ul>
-        {ingredients.map(ingredient => (
-          <li key={ingredient}>{ingredient}</li>
-        ))}
-      </ul>
-    </div>
-  )
+const poem = {
+  lines: [
+    'I write, erase, rewrite',
+    'Erase again, and then',
+    'A poppy blooms.',
+  ],
 }
 
-export default function RecipeList() {
+export default function Poem() {
   return (
-    <div>
-      <h1>Recipes</h1>
-      {recipes.map(recipe => (
-        <Recipe {...recipe} key={recipe.id} />
+    <article>
+      {poem.lines.map((line, index) => (
+        <p key={index}>{line}</p>
       ))}
-    </div>
+    </article>
   )
-}
-
-{
-  /* <div className='p-4 mt-4'>
-      <h1 className='mt-4 text-2xl font-bold'>Recipes</h1>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h2 className='mt-8 text-lg font-base font-medium'>{recipe.name}</h2>
-          <ul className='p-4 mt-2 text-base font-light list-disc'>
-            {recipe.ingredients.map(ingredient => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div> */
 }
