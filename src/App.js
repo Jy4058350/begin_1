82425076a3e0fa11e2af3b9dd861fe4f6291680e
Jy4058350 <j_yamabe@mac.com>
2024-08-28 12:30:40 +0900

    チャレンジ二問目first

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import { people } from './data.js'
-import { getImageUrl } from './utils.js'
+import { recipes } from './data.js'
 
-export default function List() {
-  const chemists = people.filter(person => person.profession === 'chemist')
-  const others = people.filter(person => person.profession !== 'chemist')
+export default function RecipeList() {
   return (
-    <article>
-      <h1>Scientists</h1>
-      <h2>Chemist</h2>
-      <ul>
-        {chemists.map(person => (
-          <li key={person.id}>
-            <img src={getImageUrl(person)} alt={person.name} />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        ))}
-      </ul>
-      <h1>Scientists</h1>
-      <h2>Other</h2>
-      <ul>
-        {others.map(person => (
-          <li key={person.id}>
-            <img src={getImageUrl(person)} alt={person.name} />
-            <p>
-              <b>{person.name}:</b>
-              {' ' + person.profession + ' '}
-              known for {person.accomplishment}
-            </p>
-          </li>
-        ))}
-      </ul>
-    </article>
+    <div>
+      <h1>Recipes</h1>
+    </div>
   )
 }
