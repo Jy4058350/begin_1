bc0087b2b555d8f48c025c10d32750c6e0d21fa2
Jy4058350 <j_yamabe@mac.com>
2024-08-29 19:06:33 +0900

    Header と Avatar にすべての情報を props で伝えます

--- src/profile.js
+++ src/profile.js
@@ -1,29 +1,26 @@
 import Panel from './Panel.js'
 import { getImageUrl } from './utils.js'
 
-let currentPerson
-
 export default function Profile({ person }) {
   // console.log(person.imageId)
   return (
     <Panel>
-      <Header name={person.name} />
-      <Avatar id={person} name={person.name} />
+      <Header person={person} />
+      <Avatar person={person} />
     </Panel>
   )
 }
 
-function Header({ name }) {
-  return <h1>{name}</h1>
+function Header({ person }) {
+  return <h1>{person.name}</h1>
 }
 
-function Avatar({ id, name }) {
-  console.log(id)
+function Avatar({ person }) {
   return (
     <img
       className='avatar'
-      src={getImageUrl(id)}
-      alt={name}
+      src={getImageUrl(person)}
+      alt={person.name}
       width={50}
       height={50}
     />
