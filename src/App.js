162d18c977dc82aacfb9ba8a8d5e24d4cc405230
Jy4058350 <j_yamabe@mac.com>
2024-08-29 14:25:24 +0900

    レンダー中にその場で作成した変数やオブジェクトであれば、書き換えることは全く問題ない

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-let guest = 0
-
 function Cup({ guest }) {
   return <h2>Tea cup for guest #{guest}</h2>
 }
 
-export default function TeaSet() {
-  return (
-    <>
-      <Cup guest={1} />
-      <Cup guest={2} />
-      <Cup guest={3} />
-    </>
-  )
+export default function TeaGathering() {
+  let cups = []
+  for (let i = 1; i <= 12; i++) {
+    cups.push(<Cup key={i} guest={i} />)
+  }
+  return cups
 }
