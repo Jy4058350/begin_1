ea01f102b4a1f36b22468a209d2e6637fc63031a
Jy4058350 <j_yamabe@mac.com>
2024-08-29 14:13:55 +0900

    propsを使ってguestを渡すようにして修正

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-function Recipe({ drinkers }) {
-  return (
-    <ol className='ml-4 list-decimal'>
-      <li className='  text-base font-normal'>
-        Boil {drinkers} cups of water.
-      </li>
-      <li className=' text-base font-normal '>
-        Add {drinkers} spoons of tea and {0.5 * drinkers} spoons of spice.
-      </li>
-      <li className=' text-base font-normal'>
-        Add {0.5 * drinkers} cups of milk to boil and sugar to taste.
-      </li>
-    </ol>
-  )
+let guest = 0
+
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>
 }
 
-export default function App() {
+export default function TeaSet() {
   return (
-    <section className=' p-4'>
-      <h1 className='mb-4 text-xl font-bold'>Spiced Chai Recipe</h1>
-      <h2 className='mb-4 text-lg font-semibold'>For two</h2>
-      <Recipe drinkers={2} />
-      <h2 className='mb-4 mt-4 text-lg font-semibold'>For a gathering</h2>
-      <Recipe drinkers={4} />
-    </section>
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
   )
 }
