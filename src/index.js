116c49f898ba279c2bfa22ba49c04e32c073fbf9
Jy4058350 <j_yamabe@mac.com>
2024-08-29 19:11:41 +0900

    Add StrictMode

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ReactDOM from 'react-dom/client'
+import ReactDOM, { StrictMode } from 'react-dom/client'
 import App from './App.js'
 import './styles.css'
 
 const app = ReactDOM.createRoot(document.getElementById('app'))
 
-app.render(<App />)
+app.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
