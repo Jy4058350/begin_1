c5112c9b6317a48f0996df192ac88a9f66e6a92f
Jy4058350 <j_yamabe@mac.com>
2024-08-30 08:44:02 +0900

    解答

--- src/StoryTray.js
+++ src/StoryTray.js
@@ -1,15 +1,10 @@
 export default function StoryTray({ stories }) {
-  const stories1 = []
-  stories1.push({
-    id: 'create',
-    label: 'Create Story',
-  })
-  const nextStories = stories.push(stories1)
   return (
     <ul>
-      {nextStories.map(story => (
+      {stories.map(story => (
         <li key={story.id}>{story.label}</li>
       ))}
+      <li>Create Story</li>
     </ul>
   )
 }
