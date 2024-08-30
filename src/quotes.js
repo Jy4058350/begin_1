cc15fd44e9dfb92a87ced9741c70d762a32fd1db
Jy4058350 <j_yamabe@mac.com>
2024-08-30 09:55:08 +0900

    Add Copyright.js, InspirationGenerator.js, quotes.js, in src

---
+++ src/quotes.js
@@ -0,0 +1,5 @@
+export default [
+  'Don’t let yesterday take up too much of today.” - Will Rogers',
+  'Amibition is putting a ladder against the sky.',
+  "A joy that's shared is a joy made double.",
+]
