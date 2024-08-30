5f254a2d8955f2ea5a6636351c912705c2636c02
Jy4058350 <j_yamabe@mac.com>
2024-08-30 09:35:05 +0900

    Add FancyText jsFile

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import FancyText from './FancyText'
+import InspirationGenerator from './InspirationGenerator'
+import Copyright from './Copyright'
+
 export default function App() {
   return <h1 className='text-red-300 text-3xl'>Hellow React!</h1>
 }
