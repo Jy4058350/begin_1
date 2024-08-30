c4cd4c7e8af540893af7f8b61d87d20881e13ea3
Jy4058350 <j_yamabe@mac.com>
2024-08-30 13:16:43 +0900

    Step3 is adding onClick={handleClick} to JSX

--- src/App.js
+++ src/App.js
@@ -2,5 +2,5 @@ export default function Button() {
   function handleClick() {
     alert('You clicked me!')
   }
-  return <button>I don't do something</button>
+  return <button onClick={handleClick}>I don't do something</button>
 }
