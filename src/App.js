3d7f18260fff55c2e3d56d0b8a0f3bd451cbb9b9
Jy4058350 <j_yamabe@mac.com>
2024-08-30 13:34:46 +0900

    selfProduct

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+export default function Toolbar() {
+  return (
+    <div>
+      <AlertButton message='Playing' button='Play Movie' />
+      <AlertButton message='Uploading' button='Upload Image' />
+    </div>
+  )
+}
 
+function AlertButton({ message, button }) {
+  return <button onClick={() => alert(`${message}`)}>{button}</button>
+}
