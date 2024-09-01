bf3cb9f3fa788e3019bcce6a3f2da3a05289f814
Jy4058350 <j_yamabe@mac.com>
2024-09-01 21:47:25 +0900

    self-product-failed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
 // ヒント　２種類のボタンをつくる。
 // 例えば動画再生用とアップロード用のボタンをコンポーネント化する
 
+// 次のステップ
+// ボタンをつくるコンポーネントを検討する
+
 export default function Toolbar() {
   return (
     <div>
-      <Button onClick={PlayClick}>Playing Kiki's Delivery Service</Button>
-      <Button onClick={UploadClick}>Upload Image</Button>
+      <PlayButton>Playing Kiki's Delivery Service</PlayButton>
+      <UploadButton>Upload Image</UploadButton>
     </div>
+  )
+}
+
+function PlayButton({ children }) {
+  const PlayClick = () => {
+    return alert('Playing Kikis Delivery Service!')
+  }
+  return <Button onClick={PlayClick} children={children} />
+}
+function UploadButton({ children }) {
+  return (
+    <Button
+      onClick={() => {
+        return alert('Uploading!')
+      }}
+      children={children}
+    />
   )
 }
 
 function Button({ onClick, children }) {
   return <button onClick={onClick}>{children}</button>
-}
-
-const PlayClick = () => {
-  return alert('Playing Kikis Delivery Service!')
-}
-
-const UploadClick = () => {
-  return alert('Uploading!')
 }
