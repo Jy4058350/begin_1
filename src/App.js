328675300408b69e9112d92b1d132971e1e139bc
Jy4058350 <j_yamabe@mac.com>
2024-09-01 21:53:50 +0900

    playとタイトルを分離できることを検討したほうがよい

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 // ヒント　２種類のボタンをつくる。
+// Step1
 // 例えば動画再生用とアップロード用のボタンをコンポーネント化する
 
-// 次のステップ
+// Step2
 // ボタンをつくるコンポーネントを検討する
+
+// Step3
+// ボタンの名称もボタンごとに確定しているはず
 
 export default function Toolbar() {
   return (
     <div>
-      <PlayButton>Playing Kiki's Delivery Service</PlayButton>
-      <UploadButton>Upload Image</UploadButton>
+      <PlayButton />
+      <UploadButton />
     </div>
   )
 }
 
-function PlayButton({ children }) {
+function PlayButton() {
   const PlayClick = () => {
     return alert('Playing Kikis Delivery Service!')
   }
-  return <Button onClick={PlayClick} children={children} />
+  return <Button onClick={PlayClick}>Playing Kiki's Delivery Service</Button>
 }
-function UploadButton({ children }) {
+function UploadButton() {
   return (
     <Button
       onClick={() => {
         return alert('Uploading!')
       }}
-      children={children}
-    />
+    >
+      UploadImage
+    </Button>
   )
 }
 
