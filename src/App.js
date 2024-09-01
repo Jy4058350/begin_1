2f8c59dd5499b31aa47f3f60325067c321d32187
Jy4058350 <j_yamabe@mac.com>
2024-09-01 21:27:41 +0900

    イベントハンドラをpropsとして渡す---failed

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
+const messages = [
+  {
+    id: 0,
+    message: 'Playing!',
+  },
+  {
+    id: 1,
+    message: 'Uploading!',
+  },
+]
+
 export default function Toolbar() {
   return (
     <div>
-      <PlayButton movieName='Kikis Deleveryy Service'>
-        Play "Kiki's Delivery Service"
-      </PlayButton>
-      <UploadButton>Upload Image</UploadButton>
+      <Button onClick={PlayClick}>Playing Kiki's Delivery Service</Button>
+      <Button onClick={UploadClick}>Upload Image</Button>
     </div>
-  )
-}
-
-function PlayButton({ movieName, children }) {
-  function handlePlayClick() {
-    alert(`Playing ${movieName}`)
-  }
-  return (
-    <>
-      <Button onClick={handlePlayClick} children={children} />
-    </>
-  )
-}
-
-function UploadButton({ children }) {
-  function handlePlayClick() {
-    alert('Uploading')
-  }
-  return (
-    <>
-      <Button onClick={handlePlayClick} children={children} />
-    </>
   )
 }
 
 function Button({ onClick, children }) {
   return <button onClick={onClick}>{children}</button>
+}
+
+const PlayClick = () => {
+  return alert('Playing Kikis Delivery Service!')
+}
+
+const UploadClick = () => {
+  return alert('Uploading!')
 }
