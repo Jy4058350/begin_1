6f4ea7328cc63af4063872b95653345304b42146
Jy4058350 <j_yamabe@mac.com>
2024-09-03 20:33:19 +0900

    self-product is fail

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import { sculptureList } from './data.js'
 
 export default function Gallery() {
   const [index, setIndex] = useState(0)
+  const [show, setShow] = useState(false)
 
   function handleClick() {
     setIndex(index + 1)
+  }
+
+  function handleShow() {
+    setShow(show ? false : true, false)
+    console.log(show)
+    if (show) {
+    }
   }
 
   let sculpture = sculptureList[index]
@@ -20,8 +28,9 @@ export default function Gallery() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
+      <button onClick={handleShow}>Show detail</button>
       <p>{sculpture.description}</p>
+      <img src={sculpture.url} alt={sculpture.alt} />
     </>
   )
 }
