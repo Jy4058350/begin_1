a298736f6cb33cb9f409207d058f0e732c608273
Jy4058350 <j_yamabe@mac.com>
2024-09-04 17:18:51 +0900

    self-product failed

--- src/processQueue.js
+++ src/processQueue.js
@@ -1,7 +1,14 @@
 export function getFinalState(baseState, queue) {
   let finalState = baseState
 
-  // TODO: do something with the queue...
-
+  queue.map(q => {
+    if (typeof q !== 'object') {
+      finalState = baseState + q
+      console.log(finalState)
+    } else {
+      console.log(q)
+    }
+    return finalState
+  })
   return finalState
 }
