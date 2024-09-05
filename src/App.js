234d5611d8656bc9cab851ea507e799584a13687
Jy4058350 <j_yamabe@mac.com>
2024-09-05 09:28:00 +0900

    復習

--- src/App.js
+++ src/App.js
@@ -1,49 +1,17 @@
-import { getFinalState } from './processQueue.js'
+import { useState } from 'react'
 
-function increment(n) {
-  return n + 1
-}
-increment.toString = () => 'n => n+1'
+export default function Counter() {
+  const [number, setNumber] = useState(0)
 
-export default function App() {
+  function handleClick() {
+    setNumber(5)
+    setNumber(n => n + 1)
+  }
+
   return (
     <>
-      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
-      <hr />
-      <TestCase
-        baseState={0}
-        queue={[increment, increment, increment]}
-        expected={3}
-      />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment]} expected={6} />
-      <hr />
-      <TestCase baseState={0} queue={[5, increment, 42]} expected={42} />
-    </>
-  )
-}
-
-function TestCase({ baseState, queue, expected }) {
-  const actual = getFinalState(baseState, queue)
-  return (
-    <>
-      <p>
-        Base state: <b>{baseState}</b>
-      </p>
-      <p>
-        Queue: <b>[{queue.join(', ')}]</b>
-      </p>
-      <p>
-        Expected result: <b>{expected}</b>
-      </p>
-      <p
-        style={{
-          color: actual === expected ? 'green' : 'red',
-        }}
-      >
-        Your result: <b>{actual}</b> (
-        {actual === expected ? 'correct' : 'wrong'})
-      </p>
+      <h1>{number}</h1>
+      <button onClick={handleClick}>Increase the number</button>
     </>
   )
 }
