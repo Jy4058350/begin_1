2e899e26a67476d2c6e48158edce0b312ef8cd98
Jy4058350 <j_yamabe@mac.com>
2024-09-09 10:58:13 +0900

    first:簡単な例を表示

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
-import { useImmer } from 'use-immer'
-import Background from './Background.js'
-import Box from './Box.js'
+import React, { useState, useEffect } from 'react'
 
-const initialPosition = {
-  x: 0,
-  y: 0,
-}
+function ExampleComponent() {
+  const [count, setCount] = useState(0)
 
-export default function Canvas() {
-  const [shape, updateShape] = useImmer({
-    color: 'orange',
-    position: initialPosition,
-  })
-
-  function handleMove(dx, dy) {
-    updateShape(draft => {
-      draft.position.x += dx
-      draft.position.y += dy
-    })
-  }
-
-  function handleColorChange(e) {
-    updateShape(draft => {
-      draft.color = e.target.value
-    })
-  }
+  useEffect(() => {
+    document.title = `You clicked ${count} times`
+  }, [count])
 
   return (
-    <>
-      <select value={shape.color} onChange={handleColorChange}>
-        <option value='orange'>orange</option>
-        <option value='lightpink'>lightpink</option>
-        <option value='aliceblue'>aliceblue</option>
-      </select>
-      <Background position={initialPosition} />
-      <Box color={shape.color} position={shape.position} onMove={handleMove}>
-        Drag me!
-      </Box>
-    </>
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
   )
 }
+
+export default ExampleComponent
